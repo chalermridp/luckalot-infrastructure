b11b9256e0d254931a09db544bf957d471007a5c
Chalermrid Patipanang <chalermrid.patipanang@tesco.com>
2021-01-12 23:02:48 +0700

    update stack name and s3 bucket name in deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 StageName=$1
 mkdir -p artifact
 
-aws cloudformation package --template-file template.yaml --s3-bucket luckalot-infrastructure.${StageName:-"dev"}-artifact --output-template-file artifact/template.${StageName:-"dev"}.out.yaml --profile=luckalot.${StageName:-"dev"}
-aws cloudformation deploy --template-file artifact/template.${StageName:-"dev"}.out.yaml --stack-name luckalot-infrastructure-${StageName:-"dev"}-stack --capabilities CAPABILITY_NAMED_IAM --profile=luckalot.${StageName:-"dev"}
+aws cloudformation package --template-file template.yaml --s3-bucket luckalot-infrastructure-artifact --output-template-file artifact/template.${StageName:-"dev"}.out.yaml --profile=luckalot.${StageName:-"dev"}
+aws cloudformation deploy --template-file artifact/template.${StageName:-"dev"}.out.yaml --stack-name luckalot-infrastructure-stack --capabilities CAPABILITY_NAMED_IAM --profile=luckalot.${StageName:-"dev"}
